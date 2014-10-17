1ae481a1fb765928e2595a65c4a3178d53b27251
bensaintsever <benjamin.saint-sever@etu.u-bordeaux.fr>
2014-10-17 15:34:56 +0200

    Ajout des sources

---
+++ src/aquarium/Main.java
@@ -0,0 +1,16 @@
+package aquarium;
+
+import aquarium.gui.AquariumWindow;
+import aquarium.gui.Aquarium;
+
+/**
+ * Starting point of the Aquarium application
+ */
+public class Main  {
+	
+	public static void main(String[] args) {
+		Aquarium aquarium = new Aquarium();
+		AquariumWindow animation = new AquariumWindow(aquarium);
+		animation.displayOnscreen();
+	}
+}
