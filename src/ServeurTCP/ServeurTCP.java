8c03643686f650fc6073687c6d77b8645e21724b
bensaintsever <benjamin.saint-sever@etu.u-bordeaux.fr>
2014-10-24 14:23:13 +0200

    serveur tcp

--- src/ServeurTCP/ServeurTCP.java
+++ src/ServeurTCP/ServeurTCP.java
@@ -1,4 +1,8 @@
 package ServeurTCP;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,9 +20,11 @@ public class ServeurTCP {
 			InputStream is = client.getInputStream();
 			BufferedReader br = new BufferedReader(new InputStreamReader(is, "utf-8"));
 			
-			OutputStream os = client.getOutputStream()
+			OutputStream os = client.getOutputStream();
 			
-		}
+			client.close();
+			
+			}
 		
 		}catch(Exception e){}
 		
