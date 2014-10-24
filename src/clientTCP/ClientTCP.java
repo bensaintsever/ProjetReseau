586e54c317cea872eb853b1b347f7eb9bf5ed0be
sezeng <sezen.leblay@etu.u-bordeaux.fr>
2014-10-24 14:31:27 +0200

    client TCP

--- src/clientTCP/ClientTCP.java
+++ src/clientTCP/ClientTCP.java
@@ -1,5 +1,7 @@
 package clientTCP;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,9 +11,16 @@ public class ClientTCP {
 		try {
 			ServerSocket s = new ServerSocket(server);
 
-			Socket receive = s.accept();
+			Socket client = s.accept();
+			
+			OutputStream os = client.getOutputStream();
+			InputStream is = client.getInputStream();
+			os.write('a');
+			System.out.println(is.read());
+			
 			
 			s.close();
+			client.close();
 			} catch(Exception e) {
 				System.exit(0);	
 			}
