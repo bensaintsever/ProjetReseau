4c447d7b08c3e0d576ee2abfb6cd850d5b0cdfc8
bensaintsever <benjamin.saint-sever@etu.u-bordeaux.fr>
2014-12-02 16:48:42 +0100

    optimisation

--- src/Protocole/Protocole.java
+++ src/Protocole/Protocole.java
@@ -11,35 +11,40 @@ package Protocole;
  */
 public class Protocole {
     
+    public Protocole(){
+        
+    }
+    
     //addFish!numFish!width!x!y 
-    private String addFish(int IdFish, int width, int x, int y){
+    
+    public String addFish(int IdFish, int width, int x, int y){
         
         return "aquarium!addFish!"+Integer.toString(IdFish)+Integer.toString(width)+Integer.toString(x)+Integer.toString(y);
     }
     
     //removeFish!numFish
-    private String removeFish(int IdFish){
+    public String removeFish(int IdFish){
         return "aquarium!removefish"+IdFish;
     }
     
     //moveFish!numFish!x!y 
-    private String moveFish(int IdFish, int x, int y){
+    public String moveFish(int IdFish, int x, int y){
         
         return "aquarium!moveFish!"+Integer.toString(IdFish)+Integer.toString(x)+Integer.toString(y);
     }
      
     //setFishWidth!numFish!width
-    private String setFishWidth(int IdFish, int width){
+    public String setFishWidth(int IdFish, int width){
         return "aquarium!setFish!"+Integer.toString(IdFish)+Integer.toString(width);
     }
      
      //addStone!width!x!y 
-    private String addStone(int width, int x, int y){
+    public String addStone(int width, int x, int y){
         return "aquarium!addStone!"+Integer.toString(width)+Integer.toString(x)+Integer.toString(y);
     }
      
     //addWeed!width!x!y 
-    private String addWeed(int width, int x, int y){
+    public String addWeed(int width, int x, int y){
         return "aquarium!addWeed!"+Integer.toString(width)+Integer.toString(x)+Integer.toString(y);
     }
     
